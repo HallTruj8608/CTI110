e41831d727faa4560f8c430c6574b6e1122fa2c0
HallTruj8608 <halltruj8608@student.faytechcc.edu>
2025-02-23 21:19:35 -0500

    Add files via upload

---
+++ P1Lab1_HallTruongJordan.py
@@ -0,0 +1,11 @@
+# Your Name
+# Date
+# P1LAB1
+# This program prompts the user to enter their first and last name separately 
+# and then displays a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
